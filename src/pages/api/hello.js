30c6e8a80320e33ccbc4058c539d0e4d86a1c799
Zaki-88 <118656809+Zaki-88@users.noreply.github.com>
2023-02-01 15:54:36 +0700

    Added about page

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import '@/styles/globals.css'
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
