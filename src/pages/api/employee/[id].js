229d8e9b24bfd71e772b55c6dacd466c0082b3fe
Zaki-88 <118656809+Zaki-88@users.noreply.github.com>
2023-02-11 17:10:30 +0700

    url

--- src/pages/api/employee/[id].js
+++ src/pages/api/employee/[id].js
@@ -2,10 +2,7 @@ import path from "path";
 import { promises as fs } from "fs";
 export default async function handler(req, res) {
   const jsonDirectory = path.join(process.cwd(), "data");
-  const fileContents = await fs.readFile(
-    jsonDirectory + "/employees.json",
-    "utf8"
-  );
+  const fileContents = await fs.readFile(jsonDirectory + "/employees.json", "utf8");
   const employees = JSON.parse(fileContents);
 
   const e = employees.find((employee) => employee.id == req.query.id);
