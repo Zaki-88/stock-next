c5f371546454e20d7b332c06c2561e7067a162bd
Zaki-88 <118656809+Zaki-88@users.noreply.github.com>
2023-02-22 21:19:47 +0700

    stocks

--- pages/api/stocks/products/[id].js
+++ pages/api/stocks/products/[id].js
@@ -5,18 +5,23 @@ const connectionString = process.env.MONGODB_URI
 export default async function handler(req, res) {
   await connect(connectionString);
   console.log("req.method: ", req.method)
+  console.log("req.query.id", req.query.id)
 
+  const id = req.query.id
   if (req.method === 'GET') {
-    const docs = await Product.find()
-    console.log(docs)
-    res.status(200).json(docs)
-  } else if (req.method === 'POST') {
+    // Get only one document
+    const doc = await Product.findOne({ _id: id })
+    res.status(200).json(doc)
+  } else if (req.method === 'DELETE') {
+    const deletedDoc = await Product.deleteOne({ _id: id })
+    res.status(200).json(deletedDoc)
+  } else if (req.method === 'PUT') {
+    console.log('id', req.query.id)
     console.log(req.body)
-    // res.status(200).json(req.body)
-    const doc = await Product.create(req.body)
-    res.status(201).json(doc)
+    const updatedDoc = await Product.updateOne({ _id: id }, req.body)
+    res.status(200).json()
   } else {
-    res.setHeader('Allow', ['GET', 'POST'])
+    res.setHeader('Allow', ['GET', 'DELETE'])
     res.status(405).end(`Method ${req.method} Not Allowed`)
   }
 }
